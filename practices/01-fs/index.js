76b61473208c62f805a7acbc06e3d69bcfa1e939
Arturo Martínez Díaz <arturo.martinez.diaz@bbva.com>
2018-05-17 10:45:38 +0200

    feature (01-fs): index.js

---
+++ practices/01-fs/index.js
@@ -0,0 +1,8 @@
+const fs        = require('fs');
+const path      = require('path');
+const charset   = 'utf-8';
+
+//1. Get "data.txt" file path
+//2. Read file "data.txt"
+//3. Parse content from "data.txt" and convert it to JSON array
+//4. Write "data.json" file with converted content.
