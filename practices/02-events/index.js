b591f7b377e2615185bc7eadb80b0d9c1ed01c61
Arturo Martínez Díaz <arturo.martinez.diaz@bbva.com>
2018-05-17 10:54:39 +0200

    feature (02-events): new practice

---
+++ practices/02-events/index.js
@@ -0,0 +1,4 @@
+const EventsEmitter = require('events');
+
+//1. Create two classes, on will extends EventsEmitter
+//2. Communicate both classes, so after creating and running first one, second is created
