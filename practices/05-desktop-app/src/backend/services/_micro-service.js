495a010749ae6f50cfb3363c98befe84dfcfa4fc
Arturo Martínez Díaz <arturo.martinez.diaz@bbva.com>
2018-05-25 12:09:53 +0200

    feature (src/backend/services/_micro-service): avoid CORS

--- practices/05-desktop-app/src/backend/services/_micro-service.js
+++ practices/05-desktop-app/src/backend/services/_micro-service.js
@@ -20,6 +20,13 @@ class MicroService extends EventsEmitter {
     }
     start() {
         this._app = express();
+        this._app.use(this._corsHeaders);
+    }
+    _corsHeaders(req = null, res = null, next = null) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        res.setHeader('Content-Type', 'application/json;charset=utf-8');
+        next();
     }
     _createFileManager() {
         this._fm = new FileManager();
@@ -33,7 +40,7 @@ class MicroService extends EventsEmitter {
     _log(type = 'error', message = '') {
         this.emit(type, `[${this.name}] ${message}`);
     }
-    _handleRequest() {
+    _handleRequest(req = null, res = null) {
         this.error('Method "_handleRequest" must be implemented');
     }
 }
